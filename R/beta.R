fbbaa0be6a72a4fd0b94b83409e8d5402685c4a0
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-07-12 14:30:11 -0600

    Added beta() and corresponding tests.

---
+++ R/beta.R
@@ -0,0 +1,71 @@
+#' Compute the \eqn{β} (beta) parameter for the SiGN model
+#'
+#' Computes the \eqn{β} adjustment for signalled alternatives, reflecting the
+#' balance between conditional and terminal (i.e., primary/unconditional)
+#' reinforcement, as described in Equation 6 of Dunn et al. (2024).
+#'
+#' @param use_beta Logical vector indicating whether the \eqn{β} parameter
+#' should be applied.
+#' @param sig Logical vector indicating whether the alternative is signalled.
+#' See [sig_check].
+#' @param sched1 Character vector indicating the schedule type for the first
+#' alternative's initial link. See Details.
+#'  (e.g., `"VI"`, `"FR"`).
+#' @param sched2 Character vector indicating the schedule type for the second
+#' alternative's initial link. See Details.
+#' @param il_dur1 Numeric vector of durations for the first alternative's
+#' initial link.
+#' @param il_dur2 Numeric vector of durations for the second alternative's
+#' initial link.
+#' @param s_plus_dur Numeric vector of durations associated with alternative
+#' 1's \eqn{S^{+}} stimulus.
+#'
+#' @details
+#' Although the initial link durations for both alternatives are provided as
+#' arguments, the function computes \eqn{β} specifically for the first initial
+#' link.
+#'
+#' If both initial links are variable interval (VI) schedules, a common initial
+#' link duration is computed: \deqn{1 / (1 / d_1 + 1 / d_2)}
+#' Otherwise, the duration of the first initial link is used.
+#'
+#' See [choice_params] for additional details appropriate on schedule selection.
+#'
+#' @returns
+#' A numeric vector containing the \eqn{β} values.
+#'
+#' @references
+#' Dunn, R. M., Pisklak, J. M., McDevitt, M. A., & Spetch, M. L. (2024).
+#' Suboptimal choice: A review and quantification of the signal for good news
+#' (SiGN) model. *Psychological Review*. *131*(1), 58-78.
+#' \url{https://doi.org/10.1037/rev0000416}
+#'
+#'
+#' @examples
+#' beta(
+#'   use_beta = TRUE,
+#'   sig = TRUE,
+#'   sched1 = "FR",
+#'   sched2 = "FR",
+#'   il_dur1 = 1,
+#'   il_dur2 = 1,
+#'   s_plus_dur = 10
+#' )
+#' @export
+#-------------------------------------------------------------------------------
+beta <- function(use_beta, sig,
+                 sched1, sched2,
+                 il_dur1, il_dur2,
+                 s_plus_dur) {
+  # Adjust il duration if common VI
+  eff_il <- ifelse(sched1 == "VI" & sched2 == "VI",
+    1 / (1 / il_dur1 + 1 / il_dur2),
+    il_dur1
+  )
+
+  apply_beta <- sig & use_beta
+  ifelse(apply_beta,
+    log10(1 + s_plus_dur / eff_il),
+    1
+  )
+}

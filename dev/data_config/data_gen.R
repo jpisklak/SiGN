4da31fddb55943bc62100d587847737fec47d08b
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-07-20 21:20:03 -0600

    Renamed directories for clarity

--- dev/data_config/data_gen.R
+++
@@ -1,6 +0,0 @@
-subopt_full <- read.csv("C:/Users/piskl/Desktop/Projects/SiGN/dev/data_config/gen_data/Dunn_et_al_2024.csv")
-use_data(subopt_full, overwrite = TRUE)
-
-
-subopt_avian <- read.csv("C:/Users/piskl/Desktop/Projects/SiGN/dev/data_config/gen_data/Dunn_et_al_2024_modeled_subset.csv")
-use_data(subopt_avian, overwrite = TRUE)

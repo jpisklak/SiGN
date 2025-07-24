7d16c04482999c6cd486e18f9d0697cc392249c8
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-07-23 23:33:18 -0600

    Moved dev files to personal directory

--- dev/Cunningham_Shahan_2019/img_digitize.R
+++
@@ -1,8 +0,0 @@
-library(digitize)
-
-rat <- "K30"
-img <- digitize(paste0("images/", rat, ".png"))
-
-img$x <- c(10, 20, 30, 40, 50)
-img$subject <- rat
-write.csv(img, paste0("digi_data/", rat, ".csv"), row.names = FALSE)
